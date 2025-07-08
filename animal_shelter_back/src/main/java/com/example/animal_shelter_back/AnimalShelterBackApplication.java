2d1341a4434ccd6ac973de89859893c29b35e9b2
dompsz <dompszcz@gmail.com>
2025-07-08 21:29:21 +0200

    empty project

---
+++ animal_shelter_back/src/main/java/com/example/animal_shelter_back/AnimalShelterBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.animal_shelter_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalShelterBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnimalShelterBackApplication.class, args);
+    }
+
+}
---
+++ animal_shelter_back/src/test/java/com/example/animal_shelter_back/AnimalShelterBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.animal_shelter_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalShelterBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
